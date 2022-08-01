290463ddb20692226ba3f44835a4e97854a94312
SNIAPA <wasilewskihuberts@gmail.com>
2022-08-01 10:15:35 +0200

    unbound <Esc> key

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -42,6 +42,7 @@ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 -- Insert --
 -- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)
+keymap("i", "<Esc>", "<Nop>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -69,3 +70,4 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+--
