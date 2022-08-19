c0a52c171cc22cf12aa3deb7415a84e8cf0104f4
SNIAPA <wasilewskihuberts@gmail.com>
2022-08-19 15:38:51 +0200

    working on the status bar

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -39,7 +39,10 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 -- Make x delete not cut
-keymap("n", "x", "_x", opts)
+-- keymap("n", "x", "_x", opts)
+-- Add a new line withiut entering insert
+keymap('n', "O","o<Esc>k", opts)
+keymap('n', "<C-o>","o<Esc>", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
@@ -72,5 +75,3 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- Add a new line withiut entering insert
-keymap('n', "O","o<Esc>k", opts)
