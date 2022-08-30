4f39bbfbc84f7219f038528fba138e3fe802b53c
SNIAPA <wasilewskihuberts@gmail.com>
2022-08-30 13:24:13 +0200

    small fix plus workspace names

--- link.sh
+++ link.sh
@@ -4,6 +4,7 @@ ln $(realpath zsh/.zshrc) $(realpath ~/.zshrc) -s
 ln $(realpath polybar) $(realpath ~/.config/polybar) -s
 ln $(realpath nvim) $(realpath ~/.config/nvim) -s
 ln $(realpath i3) $(realpath ~/.config/i3) -s
+ln $(realpath gruvbox-dark-hard.rasi) $(realpath ~) -s
 
 xrdb .Xresources
 
