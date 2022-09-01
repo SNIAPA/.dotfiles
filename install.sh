e6e003d219f99b503d12857dd30e3b12d5fc85c2
SNIAPA <wasilewskihuberts@gmail.com>
2022-09-01 12:07:55 +0200

    added links to gitignore

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ ln $(realpath i3) $(realpath ~/.config/i3) -s
 ln $(realpath rofi) $(realpath ~/.config/rofi) -s
 ln $(realpath gruvbox-dark-hard.rasi) $(realpath ~) -s
 
-xrdb .Xresources
+find . -type l | sed -e s'/^\.\///g' >> .gitignore
 
