558bb78e5e15394611dd8c242a94889626df36c9
SAIAN <wasilewskihubers@gmail.com>
2022-09-01 18:01:07 +0200

    alacritty

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln $(realpath polybar) $(realpath ~/.config/polybar) -s
 ln $(realpath nvim) $(realpath ~/.config/nvim) -s
 ln $(realpath i3) $(realpath ~/.config/i3) -s
 ln $(realpath rofi) $(realpath ~/.config/rofi) -s
-ln $(realpath gruvbox-dark-hard.rasi) $(realpath ~) -s
+ln $(realpath alacritty) $(realpath ~/.config/alacritty) -s
 
 find . -type l | sed -e s'/^\.\///g' > .gitignore
 
