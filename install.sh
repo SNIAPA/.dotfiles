12607cec2614c0388fe269a5d808c81be21c68ad
SNIAPA <wasilewskihuberts@gmail.com>
2022-09-01 12:16:51 +0200

    wallpaper

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ ln $(realpath i3) $(realpath ~/.config/i3) -s
 ln $(realpath rofi) $(realpath ~/.config/rofi) -s
 ln $(realpath gruvbox-dark-hard.rasi) $(realpath ~) -s
 
-find . -type l | sed -e s'/^\.\///g' >> .gitignore
+find . -type l | sed -e s'/^\.\///g' > .gitignore
 
+nitrogen --set-auto wallpaper.png
