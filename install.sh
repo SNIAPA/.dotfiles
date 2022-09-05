e1b5daf46fa683789eff7d9e074922dcd076d039
SAIAN <wasilewskihubers@gmail.com>
2022-09-05 12:59:29 +0200

    conky plus polybar tweaks

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln $(realpath nvim) $(realpath ~/.config/nvim) -s
 ln $(realpath i3) $(realpath ~/.config/i3) -s
 ln $(realpath rofi) $(realpath ~/.config/rofi) -s
 ln $(realpath alacritty) $(realpath ~/.config/alacritty) -s
+ln $(realpath conky) $(realpath ~/.config/conky) -s
 
 find . -type l | sed -e s'/^\.\///g' > .gitignore
 
