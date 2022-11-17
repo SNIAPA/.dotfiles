19e93067079347a6b5b48387c67e66a67989db6b
SNIAPA <wasilewskihubers@gmail.com>
2022-11-17 05:51:10 +0100

    fixed le treesitter

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -86,7 +86,6 @@ return packer.startup(function(use)
 	-- Treesitter
 	use({
 		"nvim-treesitter/nvim-treesitter",
-		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
 	})
 
 	-- Git
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -7,7 +7,7 @@ configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
-		enable = false, -- false will disable the whole extension
+		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
