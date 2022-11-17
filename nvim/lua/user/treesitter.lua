29d1502d1cc8d46816740068f4445e17c2d0344e
SNIAPA <wasilewskihubers@gmail.com>
2022-11-17 05:41:42 +0100

    removed commit constrain, introduced a problem with highlights

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -5,9 +5,9 @@ end
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
+	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
-		enable = true, -- false will disable the whole extension
+		enable = false, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
