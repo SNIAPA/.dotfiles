b6e41a27ed4163962d2571093f6661ba02a09f88
SNIAPA <wasilewskihuberts@gmail.com>
2023-02-27 08:51:15 +0100

    .

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -18,10 +18,19 @@ return { "williamboman/mason-lspconfig.nvim",
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+    local on_attach = function (client, bufnr)
+
+      local bufopts = { noremap=true, silent=true, buffer=bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+
+    end
+
     mason_lspconfig.setup_handlers({
       function (server_name)
         require("lspconfig")[server_name].setup{
            capabilities = capabilities,
+           on_attach = on_attach
         }
       end
     })
