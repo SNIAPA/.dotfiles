7db0cdf0f63b1f8e50f05e6da48fce040f6741a4
SNIAPA <wasilewskihuberts@gmail.com>
2023-11-30 22:20:55 +0100

    fixed some plugins

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 return { 'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+  dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
   config = function ()
     require('lualine').setup {
       options = {
