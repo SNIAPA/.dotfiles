3b345f9610a833fb0ae4c62e969076a570a34625
SNIAPA <wasilewskihuberts@gmail.com>
2024-01-12 17:46:41 +0100

    fzf telescoep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,36 @@
-return { 'nvim-telescope/telescope.nvim',
+return {
+  'nvim-telescope/telescope.nvim',
   dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build =
+      'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
   },
   config = function()
-    require('telescope').setup{
+    require('telescope').setup {
       pickers = {
         find_files = {
-          disable_devicons = true,
+          disable_devicons = false,
           previewer = false,
         },
         file_browser = {
-          disable_devicons = true,
+          disable_devicons = false,
           previewer = false,
+        }
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
         }
       }
     }
+
+    require('telescope').load_extension('fzf')
   end
 }
