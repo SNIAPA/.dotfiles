6289d3d8c036cf92fdd8197415c2088f5ef33982
oxymoron <kane.bezadas@gmail.com>
2024-10-26 07:18:52 +0200

    asd

---
+++ nvim/lua/lsp/clangd.lua
@@ -0,0 +1,52 @@
+return function()
+  local util = require('lspconfig.util')
+  local root_files = {
+    '.clangd',
+    '.clang-tidy',
+    '.clang-format',
+    'compile_commands.json',
+    'compile_flags.txt',
+    'build.sh',
+    'configure.ac',
+    'run',
+    'compile',
+  }
+  require("lspconfig").clangd.setup {
+    cmd = { "clangd",
+      "--all-scopes-completion",
+      "--background-index",
+      "--clang-tidy",
+      "--compile_args_from=filesystem",
+      "--completion-parse=always",
+      "--completion-style=bundled",
+      "--cross-file-rename",
+      "--debug-origin",
+      "--enable-config",
+      "--fallback-style=Qt",
+      "--folding-ranges",
+      "--function-arg-placeholders",
+      "--header-insertion=iwyu",
+      "--pch-storage=memory",
+      "--suggest-missing-includes",
+      "-j=4", -- number of workers
+      "--log=error",
+      -- "--resource-dir="
+    },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_dir = function(fname)
+      return util.root_pattern(root_files)(fname) or util.find_git_ancestor(fname)
+    end,
+    single_file_support = true,
+    init_options = {
+      compilationDatabasePath = vim.fn.getcwd() .. "/build",
+    },
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    on_attach = function(client, bufnr)
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    end
+
+
+  }
+end
