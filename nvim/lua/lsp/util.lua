c759a13791df9e2a5359854408af2eccaf40764f
Hubert Wasilewski <hubert.wasilewski@adtran.com>
2025-06-16 13:58:48 +0200

    .

--- nvim/lua/lsp/util.lua
+++ nvim/lua/lsp/util.lua
@@ -1,5 +1,4 @@
 local on_attach = function(client, bufnr)
-  print('LSP started for buffer: ' .. bufnr);
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
